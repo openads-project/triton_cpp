c842795075738047bbb61c343c2c0be9f0d1bbad
Fabian Thomsen <fabian.thomsen@ika.rwth-aachen.de>
2024-11-22 12:36:00 +0000

    Add separate arm path

--- cmake/TritonConfig.cmake
+++ cmake/TritonConfig.cmake
@@ -7,15 +7,17 @@ else()
     set(TRITON_CLIENT_DIR /opt/tritonclient)
 endif()
 
+set(ARM_TRITON_CLIENT_DIR ${TRITON_CLIENT_DIR}/tritonserver/clients)
+
 # find include directories
-find_path(INCLUDE_DIR grpc_client.h HINTS ${TRITON_CLIENT_DIR}/include)
+find_path(INCLUDE_DIR grpc_client.h HINTS ${TRITON_CLIENT_DIR}/include ${ARM_TRITON_CLIENT_DIR}/include)
 list(APPEND INCLUDE_DIRS ${INCLUDE_DIR})
 
 # find libraries
-find_library(GrpcLib libgrpcclient.so HINTS ${TRITON_CLIENT_DIR}/lib)
-find_library(HttpLib libhttpclient.so HINTS ${TRITON_CLIENT_DIR}/lib)
-find_library(ProtobufLib libprotobuf.a HINTS ${TRITON_CLIENT_DIR}/lib)
-find_library(ProtobufLiteLib libprotobuf-lite.a HINTS ${TRITON_CLIENT_DIR}/lib)
+find_library(GrpcLib libgrpcclient.so HINTS ${TRITON_CLIENT_DIR}/lib ${ARM_TRITON_CLIENT_DIR}/lib)
+find_library(HttpLib libhttpclient.so HINTS ${TRITON_CLIENT_DIR}/lib ${ARM_TRITON_CLIENT_DIR}/lib)
+find_library(ProtobufLib libprotobuf.a HINTS ${TRITON_CLIENT_DIR}/lib ${ARM_TRITON_CLIENT_DIR}/lib)
+find_library(ProtobufLiteLib libprotobuf-lite.a HINTS ${TRITON_CLIENT_DIR}/lib ${ARM_TRITON_CLIENT_DIR}/lib)
 
 # handle the QUIETLY and REQUIRED arguments and set *_FOUND
 include(FindPackageHandleStandardArgs)
